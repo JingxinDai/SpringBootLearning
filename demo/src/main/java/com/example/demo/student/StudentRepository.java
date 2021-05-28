29e7250aad7bf41b641d61c301f15413eea91ed4
Jingxin Dai <jingxindai@outlook.com>
2021-05-28 19:24:31 -0400

    spring boot learning proj v0

---
+++ demo/src/main/java/com/example/demo/student/StudentRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
+    //select * from student email= ?
+    @Query("select s from Student s where s.email=?1")
+    Optional<Student> findStudentByEmail(String email);
+
+    @Query("select s from Student s where s.name=?1")
+    Optional<Student> findStudentByName(String name);
+
+}
